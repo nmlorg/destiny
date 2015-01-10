bb836c25fd0dfb088bcbeb8be0d7df92a4a77549
Daniel Reed <n@ml.org>
2015-01-09 21:02:04 -0800

    Quick web UI.

---
+++ main.py
@@ -0,0 +1,59 @@
+# Copyright 2014 Daniel Reed <n@ml.org>
+
+import jinja2
+import json
+import os
+import webapp2
+import destiny
+
+
+JINJA2 = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+I = 0
+
+
+class UserPage(webapp2.RequestHandler):
+  def get(self, username):
+    user = destiny.User(username)
+
+    def F(d):
+      global I
+
+      I += 1
+
+      self.response.write(' <code style="border: 1px solid black; cursor: pointer; display: inline-block; text-align: center; height: 1em; width: 1em" onclick="var st = document.getElementById(\'i%i\').style; if (st.display == \'none\') { st.display = \'\'; this.textContent = \'-\'; } else { st.display = \'none\'; this.textContent = \'+\'; }">+</code>' % I)
+
+      if isinstance(d, dict):
+        self.response.write('<ul style="display: none" id="i%i">' % I)
+        for k, v in sorted(d.iteritems()):
+          self.response.write('<li>%s:' % (k,))
+          if isinstance(v, (dict, list, tuple)) and v:
+            F(v)
+          else:
+            self.response.write(' ' + json.dumps(v))
+          self.response.write('</li>')
+        self.response.write('</ul>')
+      else:
+        self.response.write('<ol style="display: none" id="i%i">' % I)
+        for v in d:
+          self.response.write('<li>')
+          if isinstance(v, (dict, list, tuple)) and v:
+            F(v)
+          else:
+            self.response.write(json.dumps(v))
+          self.response.write('</li>')
+        self.response.write('</ol>')
+
+    self.response.content_type = 'text/html'
+    self.response.write(JINJA2.get_template('user.html').render({'user': user}))
+    F({'simplified': user, 'raw': user.raw_account})
+
+
+class Warmup(webapp2.RequestHandler):
+  def get(self):
+    pass
+
+
+app = webapp2.WSGIApplication([
+    ('/_ah/warmup', Warmup),
+    ('/user/(.*)', UserPage),
+], debug=True)
