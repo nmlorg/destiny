d8259126aff215ed34d3c9b2fed5d64d4532f64a
Daniel Reed <dan.reed@zirtual.com>
2015-10-10 19:49:12 -0700

    Merge me.py into api.py, moving it to the / slot, making login optional.

--- dashboard/api.py
+++ dashboard/api.py
@@ -5,9 +5,13 @@ import json
 import pprint
 import webapp2
 from base import bungie
+from base.bungie import auth
 from base.bungie import destiny
 from base.bungie.destiny import manifest
 from base.bungie.destiny import user as destiny_user
+from base.util import fetch
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
 
 JINJA2 = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
@@ -54,12 +58,65 @@ class DBObjectPage(webapp2.RequestHandler):
     }))
 
 
-class IndexPage(webapp2.RequestHandler):
+class User(ndb.Model):
+  username = ndb.StringProperty()
+  bungled = ndb.TextProperty()
+  bungleatk = ndb.TextProperty()
+
+  def Activate(self):
+    fetch.SetOpener(auth.BuildOpener(self.bungled, self.bungleatk))
+
+
+class MePage(webapp2.RequestHandler):
   def get(self):
+    ae_user = users.get_current_user()
+    if ae_user is None:
+      self.response.content_type = 'text/html'
+      self.response.write(JINJA2.get_template('dashboard/me_login.html').render({
+          'breadcrumbs': (),
+          'login_url': users.create_login_url(),
+      }))
+      return
+
+    email = ae_user.email().lower()
+    user = User.get_by_id(email) or User(id=email)
+    user.Activate()
+
+    user_info = bungie.GetCurrentUser()
+    if user_info is None:
+      self.response.content_type = 'text/html'
+      self.response.write(JINJA2.get_template('dashboard/me_connect.html').render({
+          'breadcrumbs': (
+              (users.create_login_url(), email),
+          ),
+      }))
+      return
+
     self.response.content_type = 'text/html'
-    self.response.write(JINJA2.get_template('dashboard/index.html').render({
-        'breadcrumbs': (),
+    self.response.write(JINJA2.get_template('dashboard/me_index.html').render({
+        'breadcrumbs': (
+            (users.create_login_url(), email),
+        ),
+        'user_info': user_info,
     }))
+
+  def post(self):
+    ae_user = users.get_current_user()
+    email = ae_user.email().lower()
+    user = User.get_by_id(email)
+    if user is None:
+      user = User(id=email)
+
+    try:
+      bungled, bungleatk = auth.Auth(self.request.get('email'), self.request.get('password'))
+    except:
+      pass
+    else:
+      user.bungled = bungled
+      user.bungleatk = bungleatk
+      user.put()
+
+    return self.redirect('/me/')
 
 
 class UserHTMLPage(webapp2.RequestHandler):
@@ -106,7 +163,7 @@ class Warmup(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/_ah/warmup', Warmup),
-    ('/', IndexPage),
+    ('/', MePage),
     ('/db/?', DBPage),
     ('/db/([a-zA-Z]+)/?', DBBucketPage),
     ('/db/([a-zA-Z]+)/([0-9]+)/?', DBObjectPage),
