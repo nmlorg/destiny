685d26ec8387f0071054b27f615429929db0dac4
Daniel Reed <dan.reed@zirtual.com>
2015-10-15 15:33:55 -0700

    Keep track of the items count by watching for mutations, not just when ListCell.push is called. (This handles items that have been removed from a ListCell due to a drag-and-drop, for example.)

--- dashboard/table.js
+++ dashboard/table.js
@@ -62,9 +62,21 @@ nmlorg.table.ListCell = function(label) {
   this.div_ = document.createElement('div');
   this.items_ = document.createElement('div');
   this.div_.appendChild(this.items_);
+  this.itemsObserver_ = new MutationObserver(function(mutations) {
+    if (this.items_.children.length == 1) {
+      this.div_.style.cursor = '';
+      this.toggle_.style.display = 'none';
+    } else {
+      this.div_.style.cursor = 'pointer';
+      this.toggle_.style.display = '';
+      this.toggle_.textContent = this.toggle_.textContent[0] + ' ' + this.items_.children.length + ' ' + this.label_;
+    }
+  }.bind(this));
+  this.itemsObserver_.observe(this.items_, {'childList': true});
   this.toggle_ = document.createElement('div');
   this.div_.appendChild(this.toggle_);
   this.toggle_.style.display = 'none';
+  this.toggle_.textContent = '\u25b2';
   this.toggle_.addEventListener('click', function() {
     if (this.items_.children.length > 1) {
       if (this.items_.style.display == '') {
@@ -90,17 +102,6 @@ nmlorg.table.ListCell.prototype.push = function(item, sortKey) {
     }
   if (i == this.items_.children.length)
     this.items_.appendChild(item);
-
-  if (this.items_.children.length == 1) {
-    this.div_.style.cursor = '';
-    this.items_.style.display = '';
-    this.toggle_.style.display = 'none';
-  } else {
-    this.div_.style.cursor = 'pointer';
-    this.items_.style.display = 'none';
-    this.toggle_.style.display = '';
-    this.toggle_.textContent = '\u25b6 ' + this.items_.children.length + ' ' + this.label_;
-  }
 };
 
 
