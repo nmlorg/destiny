e013883fdd1a3f8062b2d70d675fcafa539614fa
Daniel Reed <dan.reed@zirtual.com>
2015-10-26 00:31:29 -0700

    Pull the special Bungie.net-specific Fetch() into a separate base.bungie.fetch module.

--- base/bungie/bungienet.py
+++ base/bungie/bungienet.py
@@ -3,42 +3,12 @@
 # Copyright 2014 Daniel Reed <n@ml.org>
 
 import logging
-import os
-import re
 import urllib
-import urlparse
-from base.util import fetch
-
-
-try:
-  API_KEY = open('bungie.key').read().strip()
-except:
-  logging.exception('Unable to read API Key from "bungie.key" (see '
-                    'https://www.bungie.net/en/Clan/Post/39966/85087279/0/0):')
-  API_KEY = ''
-
-
-BASE = 'https://www.bungie.net/platform/'
-
-
-def Fetch(suffix, *args, **kwargs):
-  data = kwargs.pop('data', None)
-  if args:
-    suffix %= args
-  elif kwargs:
-    suffix %= kwargs
-
-  url = urlparse.urljoin(BASE, suffix)
-  data = fetch.Fetch(url, data=data, headers={'X-API-Key': API_KEY})
-  if isinstance(data, dict):
-    if data.get('ErrorStatus') == 'Success':
-      return data['Response']
-  else:
-    return data
+from base.bungie import fetch
 
 
 def EquipItems(accounttype, charid, itemids):
-  return Fetch('Destiny/EquipItems/', data=json.dumps({
+  return fetch.Fetch('Destiny/EquipItems/', data=json.dumps({
       'membershipType': accounttype,
       'characterId': charid,
       'itemIds': itemids,
@@ -46,123 +16,128 @@ def EquipItems(accounttype, charid, itemids):
 
 
 def GetAccount(accounttype, accountid):
-  return Fetch('Destiny/%s/Account/%i/', accounttype, accountid)
+  return fetch.Fetch('Destiny/%s/Account/%i/', accounttype, accountid)
 
 
 def GetAccountSummary(accounttype, accountid):
-  return Fetch('Destiny/%s/Account/%i/Summary/', accounttype, accountid)
+  return fetch.Fetch('Destiny/%s/Account/%i/Summary/', accounttype, accountid)
 
 
 def GetActivityHistory(accounttype, accountid, charid, mode, count=15, page=0):
-  return Fetch('Destiny/Stats/ActivityHistory/%s/%s/%s/?mode=%s&count=%i&page=%i', accounttype,
-               accountid, charid, mode, count, page)
+  return fetch.Fetch('Destiny/Stats/ActivityHistory/%s/%s/%s/?mode=%s&count=%i&page=%i',
+                     accounttype, accountid, charid, mode, count, page)
 
 
 def GetAdminsOfGroup(groupid):
-  return Fetch('Group/%s/Admins/', groupid)
+  return fetch.Fetch('Group/%s/Admins/', groupid)
 
 
 def GetAdvisorsForCurrentCharacter(accounttype, charid):
-  return Fetch('Destiny/%s/MyAccount/Character/%s/Advisors/', accounttype, charid)
+  return fetch.Fetch('Destiny/%s/MyAccount/Character/%s/Advisors/', accounttype, charid)
 
 
 def GetAllItemsSummary(accounttype, accountid):
-  return Fetch('Destiny/%s/Account/%i/Items/', accounttype, accountid)
+  return fetch.Fetch('Destiny/%s/Account/%i/Items/', accounttype, accountid)
 
 
 def GetAvailableLocales():
-  return Fetch('GetAvailableLocales/')
+  return fetch.Fetch('GetAvailableLocales/')
 
 
 def GetBungieAccount(profileid):
-  return Fetch('User/GetBungieAccount/%s/0/', profileid)
+  return fetch.Fetch('User/GetBungieAccount/%s/0/', profileid)
 
 
 def GetCharacter(accounttype, accountid, charid):
-  return Fetch('Destiny/%s/Account/%s/Character/%i/Complete/', accounttype, accountid, charid)
+  return fetch.Fetch('Destiny/%s/Account/%s/Character/%i/Complete/', accounttype, accountid, charid)
 
 
 def GetCharacterActivities(accounttype, accountid, charid):
-  return Fetch('Destiny/%s/Account/%s/Character/%i/Activities/', accounttype, accountid, charid)
+  return fetch.Fetch('Destiny/%s/Account/%s/Character/%i/Activities/', accounttype, accountid,
+                     charid)
 
 
 def GetCharacterInventory(accounttype, accountid, charid):
-  return Fetch('Destiny/%s/Account/%s/Character/%i/Inventory/', accounttype, accountid, charid)
+  return fetch.Fetch('Destiny/%s/Account/%s/Character/%i/Inventory/', accounttype, accountid,
+                     charid)
 
 
 def GetCharacterProgression(accounttype, accountid, charid):
-  return Fetch('Destiny/%s/Account/%i/Character/%i/Progression/', accounttype, accountid, charid)
+  return fetch.Fetch('Destiny/%s/Account/%i/Character/%i/Progression/', accounttype, accountid,
+                     charid)
 
 
 def GetCharacterSummary(accounttype, accountid, charid):
-  return Fetch('Destiny/%s/Account/%s/Character/%i/', accounttype, accountid, charid)
+  return fetch.Fetch('Destiny/%s/Account/%s/Character/%i/', accounttype, accountid, charid)
 
 
 def GetCurrentUser():
-  return Fetch('User/GetBungieNetUser/')
+  return fetch.Fetch('User/GetBungieNetUser/')
 
 
 def GetDestinyAggregateActivityStats(accounttype, accountid, charid):
-  return Fetch('Destiny/Stats/AggregateActivityStats/%s/%s/%s/', accounttype, accountid, charid)
+  return fetch.Fetch('Destiny/Stats/AggregateActivityStats/%s/%s/%s/', accounttype, accountid,
+                     charid)
 
 
 def GetDestinyManifest():
-  return Fetch('Destiny/Manifest/')
+  return fetch.Fetch('Destiny/Manifest/')
 
 
 def GetDestinySingleDefinition(deftype, defid):
-  return Fetch('Destiny/Manifest/%s/%s/', deftype, defid)
+  return fetch.Fetch('Destiny/Manifest/%s/%s/', deftype, defid)
 
 
 def GetGlobalAlerts():
-  return Fetch('GlobalAlerts/')
+  return fetch.Fetch('GlobalAlerts/')
 
 
 def GetGrimoireByMembership(accounttype, accountid):
-  return Fetch('Destiny/Vanguard/Grimoire/%s/%s/', accounttype, accountid)
+  return fetch.Fetch('Destiny/Vanguard/Grimoire/%s/%s/', accounttype, accountid)
 
 
 def GetItemDetail(accounttype, accountid, charid, itemid):
-  return Fetch('Destiny/%s/Account/%s/Character/%s/Inventory/%s/', accounttype, accountid, charid, itemid)
+  return fetch.Fetch('Destiny/%s/Account/%s/Character/%s/Inventory/%s/', accounttype, accountid,
+                     charid, itemid)
 
 
 def GetMyGrimoire(accounttype):
-  return Fetch('Destiny/Vanguard/Grimoire/%s/', accounttype)
+  return fetch.Fetch('Destiny/Vanguard/Grimoire/%s/', accounttype)
 
 
 def GetPostGameCarnageReport(activityid):
-  return Fetch('Destiny/Stats/PostGameCarnageReport/%s/', activityid)
+  return fetch.Fetch('Destiny/Stats/PostGameCarnageReport/%s/', activityid)
 
 
 def GetPublicAdvisors():
-  return Fetch('Destiny/Advisors/')
+  return fetch.Fetch('Destiny/Advisors/')
 
 
 def GetPublicXurVendor():
-  return Fetch('Destiny/Advisors/Xur/')
+  return fetch.Fetch('Destiny/Advisors/Xur/')
 
 
 def HelloWorld():
-  return Fetch('HelloWorld/')
+  return fetch.Fetch('HelloWorld/')
 
 
 def SearchDestinyPlayer(username, accounttype=None):
   if not accounttype:
     accounttype = 'all'
 
-  return Fetch('Destiny/SearchDestinyPlayer/%s/%s', accounttype, username)
+  return fetch.Fetch('Destiny/SearchDestinyPlayer/%s/%s', accounttype, username)
 
 
 def SearchUsers(username):
-  return Fetch('User/SearchUsers/?' + urllib.urlencode({'q': username}))
+  return fetch.Fetch('User/SearchUsers/?' + urllib.urlencode({'q': username}))
 
 
 def Settings():
-  return Fetch('Settings/')
+  return fetch.Fetch('Settings/')
 
 
 def TransferItem(accounttype, charid, item_hash, item_id=0, quantity=1, to_vault=True):
-  return Fetch('Destiny/TransferItem/', data=json.dumps({
+  return fetch.Fetch('Destiny/TransferItem/', data=json.dumps({
       'membershipType': accounttype,
       'itemReferenceHash': item_hash,
       'itemId': item_id,
