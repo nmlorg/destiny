2afa2ba5df450a0ca18c9b4c194ac290eef05f38
Daniel Reed <n@ml.org>
2016-04-24 17:57:15 -0700

    Quick /new/ tree, working toward moving all of the manifest.GetDef calls (from destiny_user.GetSource, etc.) into separate XHRs.

--- dashboard/user/__init__.py
+++ dashboard/user/__init__.py
@@ -8,6 +8,61 @@ from base.bungie import destiny_user
 from dashboard import base_app
 
 
+NETWORK_ICONS = {
+    1: 'https://www.bungie.net/img/theme/destiny/icons/icon_xbl.png',
+    2: 'https://www.bungie.net/img/theme/destiny/icons/icon_psn.png',
+}
+
+
+class AccountInfo(base_app.RequestHandler):
+  def get(self, username, account_type, account_id):
+    account_type = long(account_type)
+    account_id = long(account_id)
+    summary = bungienet.GetAccountSummary(account_type, account_id)['data']
+    all_items = bungienet.GetAllItemsSummary(account_type, account_id)['data']
+    summary['inventory']['items'] = all_items['items']
+
+    self.response.render('dashboard/user/account_info.html', {
+        'account': {
+            'icon': NETWORK_ICONS.get(account_type),
+            'id': account_id,
+            'type': account_type,
+            'name': username,
+        },
+        'breadcrumbs': (
+            ('/new/%s/' % username, username),
+            ('/new/%s/%i/%i/' % (username, account_type, account_id),
+             '%i/%i' % (account_type, account_id)),
+        ),
+        'summary': summary,
+    })
+
+
+class AccountList(base_app.RequestHandler):
+  def get(self, username):
+    accounts = [{
+        'icon': NETWORK_ICONS.get(ent['membershipType']),
+        'id': long(ent['membershipId']),
+        'type': ent['membershipType'],
+        'name': ent['displayName'],
+    } for ent in bungienet.SearchDestinyPlayer(username)]
+
+    if len(accounts) == 1:
+      account = accounts[0]
+      return self.redirect('/new/%s/%i/%i/' % (account['name'], account['type'], account['id']))
+
+    if accounts:
+      username = accounts[0]['name']
+
+    self.response.render('dashboard/user/account_list.html', {
+        'accounts': accounts,
+        'breadcrumbs': (
+            ('/new/%s/' % username, username),
+        ),
+        'username': username,
+    })
+
+
 class IndexPage(base_app.RequestHandler):
   def get(self):
     self.response.render('dashboard/user/index.html')
@@ -77,6 +132,8 @@ app = base_app.WSGIApplication([
     ('/_ah/warmup', Warmup),
     ('/', IndexPage),
     ('/items/([0-9]+)/?', ItemPage),
+    ('/new/([a-zA-Z0-9-_ ]+)/?', AccountList),
+    ('/new/([a-zA-Z0-9-_ ]+)/([0-9]+)/([0-9]+)/?', AccountInfo),
     ('/([a-zA-Z0-9-_ ]+)', UserHTMLPage),
     ('/([a-zA-Z0-9-_ ]+)[.]json', UserJSONPage),
     ('/([a-zA-Z0-9-_ ]+)[.]py', UserPyPage),
